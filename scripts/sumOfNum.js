aefbc14ddeab3d6c9cf38ac9d7f3d60de6a08a07
Saikat Saha <saikatsaha@192.168.1.2>
2024-02-07 18:43:38 +0530

    sum of natural numbers

---
+++ scripts/sumOfNum.js
@@ -0,0 +1,10 @@
+function sumOfNumbers(n){
+    // let sum = 0;
+    // for(let i=1; i<=n; i++){
+    //     sum += i;
+    // }
+    // return sum;
+    return n*(n+1)/2;
+}
+console.log('11',sumOfNumbers(5));
+console.log('22',sumOfNumbers(7));
