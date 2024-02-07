9b03e85423a7c292fad7b559689d65bb1cca4bed
Saikat Saha <saikatsaha@192.168.1.2>
2024-02-07 19:55:00 +0530

    Digit count

---
+++ scripts/digitCount.js
@@ -0,0 +1,13 @@
+//1287 -> 4
+function digitCount(num){
+    num = Math.abs(num);
+    let count = 0;
+    while(num>0){
+        count++;
+        num = Math.floor(num / 10);
+    }
+    return count;
+}
+console.log('11',digitCount(1287));
+console.log('22',digitCount(1));
+console.log('33',digitCount(9));
