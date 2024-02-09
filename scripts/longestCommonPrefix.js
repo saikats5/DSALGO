90a0178688c880adaa690370a36cdecc944d577a
Saikat Saha <saikats_5@yahoo.co.in>
2024-02-09 08:55:06 +0530

    Longest prefix

---
+++ scripts/longestCommonPrefix.js
@@ -0,0 +1,17 @@
+//["flower","flow","flight"] -> 'fl'
+function prefix(words){
+    if(!words[0] || words.length === 1){
+        return words[0] || "";
+    }
+    let i = 0;
+    while(words[0][i] && words.every((val)=>val[i] === words[0][i])){
+        i++;
+    }
+    return words[0].substr(0, i);
+}
+console.log('11', prefix([]));
+console.log('22', prefix([""]));
+console.log('33', prefix(["abc"]));
+console.log('44', prefix(["abcdefgh", "abcde", "abe"]));
+console.log('55', prefix(["abc", "abc", "abc"]));
+console.log('66', prefix(["abc", "abcde", "xyz"]));
