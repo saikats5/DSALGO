d89a54d9c401854ba3c7f32fae7e86b482404460
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-02 11:09:49 +0530

    new code added

--- scripts/numberPalindrome.js
+++ scripts/numberPalindrome.js
@@ -1,12 +1,13 @@
 //1234 !== 4321 , 121 == 121
-function numberPalindrome(num){
-    let actualNum = num, reversedNum = 0;
-    while(actualNum>0){
-        let lastNum = actualNum%10;
-        reversedNum = (reversedNum*10) + lastNum;
-        actualNum = Math.floor(actualNum/10);
-    }
-    return num === reversedNum;
+function numberPalindrome(num) {
+  let actualNum = num,
+    reversedNum = 0
+  while (actualNum > 0) {
+    let lastNum = actualNum % 10
+    reversedNum = reversedNum * 10 + lastNum
+    actualNum = Math.floor(actualNum / 10)
+  }
+  return num === reversedNum
 }
-console.log('11',numberPalindrome(1234));
-console.log('22',numberPalindrome(121));
+console.log('11', numberPalindrome(1234))
+console.log('22', numberPalindrome(121))
